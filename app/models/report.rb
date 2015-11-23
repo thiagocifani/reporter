97e34b8cdab05b3b5101857dc25abd1805215e0f
thiagocifani <cifani.thiago@gmail.com>
2015-11-23 14:50:25 -0200

    After destroy to remove redis keys.

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,11 @@ class Report < ActiveRecord::Base
   include Redis::Objects
 
   list :pages
+
+  after_destroy :delete_redis_keys
+  def delete_redis_keys
+    self.class.redis_objects.each do |key,opts|
+      redis.del(redis_field_key(key))
+    end
+  end
 end
