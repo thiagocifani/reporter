81dd36c9439ba1518b49fa19b578fc07a49070a0
Thiago Cifani <cifani.thiago@gmail.com>
2015-12-09 12:40:09 -0200

    Changes redis default url.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@
 if ENV["REDISCLOUD_URL"]
   $redis = Redis.new(:url => ENV["REDISCLOUD_URL"], :driver => :hiredis)
 else
-  $redis = Redis.new(:url => "redis://192.168.33.12:6379", :driver => :hiredis)
+  $redis = Redis.new(:url => "redis://localhost:6379", :driver => :hiredis)
 end
